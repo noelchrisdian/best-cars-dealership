cb263d4070ac201374c850067d528191e7430902
noelchrisdian <noelchrisdian@gmail.com>
2025-03-21 10:35:43 -0400

    Fix linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,6 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarMake(models.Model):
